b9534c2a11bca0b72bcfc12a6edbb6a3aa76cceb
Diego Queiroz <diegiwg@gmail.com>
2023-02-16 19:20:40 -0300

    qusar to qausar

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from nicegui import ui
+
 from quasar import *
 
-
+# Avatar
 el_1 = avatar("clock")
 el_1.qcontent.icon("directions")
 el_1.qstyle.color("pink")
 
+# Badge
 el_2 = badge()
 el_2.qcontent.label("Hello")
 el_2.qcontent.align("center")
 el_2.qstyle.color("blue")
 el_2.qstyle.outline(True)
 
+# Banner
 el_3 = banner()
 el_3.qcontent.inline_actions(True)
 el_3.qstyle.rounded(True)
@@ -21,6 +24,7 @@ with el_3.style(add="background: red; color: white;"):
     ui.label("Hello")
     ui.label("World")
 
+# Bar
 el_4 = bar()
 el_4.qstyle.dark(True)
 el_4.qstyle.dense(True)
@@ -31,6 +35,7 @@ with el_4.style(add="background: red; color: white;"):
     ui.label("View")
     ui.label("Tools")
 
+# Breadcrumb
 el_5 = breadcrumbs()
 
 with el_5:
